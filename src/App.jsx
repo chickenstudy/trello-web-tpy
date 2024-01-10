0dea8d0fcba7c6f168e741b1f06cb65fe7997885
Chicken <tuannnhe163137@fpt.edu.vn>
2024-01-11 01:39:21 +0700

    Config_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 function App() {
   return (
     <>
       <div>tuna park yang</div>
+      <br />
       <Button variant="contained">Hello World</Button>
       <Button variant="text">Hello World</Button>
       <Button variant="outlined">Hello World</Button>
+
+      <Typography variant="body2" color="text.secondary">
+        Hello World
+      </Typography>
     </>
   );
 }
