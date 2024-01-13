5efe68fb7631824c4cfe66467f4fea5e26e90e76
Chicken <tuannnhe163137@fpt.edu.vn>
2024-01-14 00:09:08 +0700

    Refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,11 @@
-import {
-  Box,
-  Container,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import Board from "./pages/Boards/_id";
 
-import { useColorScheme } from "@mui/material/styles";
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme();
-
-  const handleChange = (event) => {
-    setMode(event.target.value);
-  };
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-            <LightModeIcon fontSize="small" /> Light
-          </div>
-        </MenuItem>
-        <MenuItem value="dark">
-          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-            <DarkModeIcon fontSize="small" /> Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value="system">
-          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-            <SettingsBrightnessIcon fontSize="small" /> System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  );
-}
-// luu dark or light mode vao local storage
 function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
-      <Box
-        sx={{
-          backgroundColor: "primary.light",
-          width: "100%",
-          height: (theme) => theme.trello.appBarHeight,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <ModeSelect />
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: "primary.dark",
-          width: "100%",
-          height: (theme) => theme.trello.boardBarHeight,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        board bar
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          width: "100%",
-          height: (theme) => theme.trello.boardContentHeight,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        board content
-      </Box>
-    </Container>
+    <>
+      {/* React router dom  */}
+      <Board />
+    </>
   );
 }
 
---
+++ src/utils/contants.js
@@ -0,0 +1,1 @@
+export const API_ADDRESS = "https://api.github.com";
