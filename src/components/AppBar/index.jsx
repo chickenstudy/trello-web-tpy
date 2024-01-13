b819c45711bb3ffed242f8dfe6f5fb8deb10cf49
Chicken <tuannnhe163137@fpt.edu.vn>
2024-01-14 03:24:25 +0700

    Relative_absolute_import

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import ModeSelect from "../ModeSelect";
+import ModeSelect from "~/components/ModeSelect";
 function AppBar() {
   return (
     <Box
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,1 @@
+// authentication - sign in sign up
