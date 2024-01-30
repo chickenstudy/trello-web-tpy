b16b0ebd30602fe273ce19bfb304239160ecfb7b
Tuan Park Yang <tuannnhe163137@fpt.edu.vn>
2024-01-31 03:33:08 +0700

    board_content_p2

---
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -0,0 +1,51 @@
+import Box from "@mui/material/Box";
+import Column from "./Column/Column";
+import Button from "@mui/material/Button";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+
+function ListColumns() {
+  return (
+    <Box
+      sx={{
+        bgcolor: " inherit",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        overflowX: "auto",
+        overflowY: "hiden",
+        "&::-webkit-scrollbar-track": {
+          m: 2,
+        },
+      }}
+    >
+      <Column />
+      <Column />
+      {/* Box add new column */}
+      <Box
+        sx={{
+          minWidth: "200px",
+          maxWidth: "200px",
+          mx: 2,
+          borderRadius: "6px",
+          height: "fit-content",
+          bgcolor: "#ffffff3d",
+        }}
+      >
+        <Button
+          startIcon={<NoteAddIcon />}
+          sx={{
+            color: "white",
+            width: "100%",
+            justifyContent: "flex-start",
+            pl: 2.5,
+            py: 1,
+          }}
+        >
+          Add new column
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default ListColumns;
